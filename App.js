ac5f2515492249831431029c44beb84d25b815d2
Christy <57055910+christyc01@users.noreply.github.com>
2021-10-05 10:45:31 -0600

    Add modal with QR code

--- App.js
+++ App.js
@@ -1,30 +1,58 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import {
+  Image,
+  Modal,
   Pressable,
+  SafeAreaView,
+  StatusBar,
   StyleSheet,
   Text,
   View,
 } from "react-native";
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text style={styles.headingText}>Vaccine Passport</Text>
       <Text style={styles.bigGerm}>🦠</Text>
-
-      <Pressable
-        onPress={() => 
-          alert("Pressed")
-        }
-        style={styles.button}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
+        }}
       >
-        <Text style={styles.buttonText}>
-          I solemnly swear that I've been vaccinated
-        </Text>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Yay, here it is!</Text>
+            <Image 
+              style={styles.qrCode}
+                source={{ 
+                  width: 300,
+                  height: 300,
+                  uri: "https://api.qrserver.com/v1/create-qr-code/?data=HelloWorld&amp;size=100x100"
+                }} 
+                /> 
+            <Pressable
+              style={styles.button}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.pressableText}>Close</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={styles.button}
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.buttonText}>If you promise you've been vaccinated, click here for a secure QR code!</Text>
       </Pressable>
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -50,15 +78,52 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "white",
     fontSize: 30,
+    fontWeight: "bold",
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
   },
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   headingText: {
     textAlign: "center",
     fontSize: 60,
     padding: 0,
   },
+  modalText: {
+    textAlign: "center",
+    fontSize: 40
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  pressableText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 30,
+  },
+  qrCode: {
+    marginTop: 40,
+    marginBottom: 40,
+  }
 });
